4c027e65b84bcc0b7fe63bb802e3d46d91c4d02f
Ahmed Abououf <aabuauf@yahoo.com>
2019-11-24 18:32:38 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,34 @@
 // Code MovieReviews Here
+import React from "react";
+
+
+// const MovieReviews = props => <div className="review-list">
+
+//     <a href={props.url}>{props.title}</a>
+// </div>
+// export default MovieReviews;
+
+const MovieReviews = props => (
+
+    <div className="review-list">
+
+        <ul>
+            {/* <a href={props.url}>{props.title}</a> */}
+            {props.reviews.map(p =>
+
+                <li>
+                    <div className="review">
+                        <span>{p.byline}</span>
+                        <span>{p.headline}</span>
+                        <span>{p.summary_short}</span>
+                    </div>
+                </li>
+
+            )}
+
+
+            {/* {console.log(props.reviews)} */}
+        </ul>
+    </div>
+)
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,58 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'IiNB7z5wuVE3jQ6NY8l77edFIqvtfNhN';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+    + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+    state = {
+        reviews: [],
+        searchTerm: ""
+    };
+
+    fetchMovies = () => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(d =>
+                //  console.log(d.results)
+                this.setState({ reviews: d.results })
+            )
+
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.fetchMovies()
+
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            [e.target.id]:
+                e.target.value
+        });
+    };
+
+    render() {
+        return <div >
+
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="searchTerm">Enter A search Term: </label>{" "}
+                <input
+                    id="searchTerm"
+                    value={this.state.searchTerm}
+                    onChange={this.handleInputChange}
+                />
+                <br />
+
+                <input type="submit" />
+            </form>
+            <MovieReviews reviews={this.state.reviews} />
+        </div>
+    }
+}
